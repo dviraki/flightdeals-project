469aa5fcea96c6321aad2b98d47c11cb0e53ae6d
dviraki <141942730+dviraki@users.noreply.github.com>
2024-05-28 12:58:41 +0300

    Update flight_data.py

--- flight-deals/flight_data.py
+++ flight-deals/flight_data.py
@@ -1,6 +1,20 @@
 class FlightData:
-    def __init__(self, price, origin_city, origin_airport, destination_city, destination_airport, out_date,
+    def _init_(self, price, origin_city, origin_airport, destination_city, destination_airport, out_date,
                  return_date, stop_overs=0, via_city=""):
+        """
+        Initializes an instance of FlightData with the provided parameters.
+
+        Parameters:
+            price (float): The price of the flight.
+            origin_city (str): The origin city of the flight.
+            origin_airport (str): The origin airport code of the flight.
+            destination_city (str): The destination city of the flight.
+            destination_airport (str): The destination airport code of the flight.
+            out_date (str): The departure date of the flight.
+            return_date (str): The return date of the flight.
+            stop_overs (int): The number of stopovers in the flight route (default is 0).
+            via_city (str): The city where the flight has stopovers (default is an empty string).
+        """
         self.price = price
         self.origin_city = origin_city
         self.origin_airport = origin_airport
@@ -10,4 +24,4 @@ class FlightData:
         self.return_date = return_date
 
         self.stop_overs = stop_overs
-        self.via_city = via_city
+        self.via_city = via_city
