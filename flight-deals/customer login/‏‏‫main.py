b01099a92ac5aa06a050791cb053c49e6040a32d
dviraki <141942730+dviraki@users.noreply.github.com>
2024-05-28 12:57:46 +0300

    Update ‏‏‫main.py

--- flight-deals/customer login/‏‏‫main.py
+++ flight-deals/customer login/‏‏‫main.py
@@ -1,24 +1,28 @@
-import sheety
+import sheety  
 
+# Introduction message
 print("Welcome to flight-101 club \n \
 We find the best flight deals and email them to you.")
 
+# Asking for user's first name and last name
 first_name = input("What is your first name? ").title()
 last_name = input("What is your last name? ").title()
 
+# Initializing email and email verification variables
 email = "email1"
-email_verifecation = "email2"
+email_verification = "email2"
 
-while email != email_verifecation:
-  email = input("What is your email? ")
-  if email.lower() == "quit" \
-         or email.lower() == "exit":
-    exit()
-  email_verifecation = input("Please verify your email : ")
-  if email_verifecation.lower() == "quit" \
-        or email_verifecation.lower() == "exit":
-    exit()      
+# Loop until email and its verification match or user opts to quit
+while email != email_verification:
+    email = input("What is your email? ")
+    if email.lower() == "quit" or email.lower() == "exit":  # Allowing the user to quit
+        exit()
+    email_verification = input("Please verify your email : ")
+    if email_verification.lower() == "quit" or email_verification.lower() == "exit":  # Allowing the user to quit
+        exit()
 
+# Confirmation message upon successful registration
 print("OK. You're in the club!")
 
-sheety.post_new_row(first_name, last_name, email)
+# Call the function to add user information to the database
+sheety.post_new_row(first_name, last_name, email)
